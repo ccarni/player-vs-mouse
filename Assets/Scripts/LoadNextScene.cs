c00147a8fab82dc16a45db0128763f37570be99b
Nicholas Carnival <36806704+nickcarnival@users.noreply.github.com>
2023-07-08 22:52:09 -0600

    cutscene, main menu, audio, animations

---
+++ Assets/Scripts/LoadNextScene.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextScene : MonoBehaviour
+{
+    public float timeToWait = 9f;
+    void Start() {
+        StartCoroutine(NextScene());
+    }
+
+    // Update is called once per frame
+    IEnumerator NextScene() 
+    {
+        yield return new WaitForSeconds(timeToWait);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
